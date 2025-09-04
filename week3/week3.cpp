4c37a87cd2e1e17f977c6401f47da532d9916daa
LeoLuttmann17 <leoluttmann17@gmail.com>
2025-09-03 21:24:10 -0700

    week 3 homework

---
+++ week3/week3.cpp
@@ -0,0 +1,23 @@
+// main.cpp
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    // Print greeting message to the user
+    cout << "Hello my friend! I know you are eager to buy some veggies, but do you know much to pay in total?\n";
+    cout << "Welcome to my Inventory CalculatorProgram.\n\n";
+ 
+    // Demonstrating escape characters
+    cout << "Here are some escape characters in action:\n";
+    cout << "\t- Tabbed line\n";
+    cout << "This is a backslash: \\\n";
+    cout << "This is a double quote: \"Hello!\"\n\n";
+ 
+    // Output sizes of basic data types
+    cout << "Data Type Sizes:\n";
+    cout << "\tSize of int: " << sizeof(int) << " bytes\n";
+    cout << "\tSize of float: " << sizeof(float) << " bytes\n";
+    cout << "\tSize of char: " << sizeof(char) << " byte\n";
+ 
+    return 0;
+}
